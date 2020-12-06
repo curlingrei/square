0d62c8fa6337ad8681015a7fe3a06158527841d5
rfujisaki <sax.friza2421db@gmail.com>
2020-12-07 01:05:11 +0900

    after rails g controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'toppages/index'
   root 'toppages#index'
+  get 'signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
