ac3a2368d93038cd0f397a9b389f3cf33b24da79
rfujisaki <sax.friza2421db@gmail.com>
2020-12-06 11:13:29 +0900

    toppage-design_first-attempt

--- app/javascript/packs/toppages/toppages.js
+++ app/javascript/packs/toppages/toppages.js
@@ -1,9 +1,17 @@
 $(function(){
 
-    $('.btn').click(function(){
-        $(this).css('background-color', 'red');
-        
-    });
+    $('.c-button').click(
+        function(){
+            // 切り替えボタンの色付け用
+            $('.clicked-btn').removeClass('clicked-btn');
+            // 画像切り替え用
+            $('.active').removeClass('active');
+            var clickedIndex = $('.c-button').index($(this));
+            $('.image').eq(clickedIndex).addClass('active');
+            $(this).addClass('clicked-btn');
+        }
+    );
+
 
 });
 
