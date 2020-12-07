72bb1242c9a4e1f0d020797f9078fafcf0a94eb8
rfujisaki <sax.friza2421db@gmail.com>
2020-12-08 01:06:54 +0900

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,22 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザ登録が完了しました'
+      redirect_to @user
+    else
+      flash[:danger] = 'ユーザの登録に失敗しました'
+      render :new
+    end
   end
 
   def edit
@@ -19,5 +29,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
