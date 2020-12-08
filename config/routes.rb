5b88d17b2481fe261b7932cdd1881e6b5a3424b9
rfujisaki <sax.friza2421db@gmail.com>
2020-12-09 00:07:15 +0900

    sessions routing

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   get 'toppages/index'
   root 'toppages#index'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
