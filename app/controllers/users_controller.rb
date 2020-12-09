1f7e201766736b7645c56e7d39f9cb97a18311ac
rfujisaki <sax.friza2421db@gmail.com>
2020-12-10 00:41:26 +0900

    user edit update destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :update, :destroy]
+  before_action :set_user, only: [:show, :edit]
+  #ゲストユーザには情報の更新や退会処理を認めない
+  before_action :guest_user?, only: [:edit, :update, :destroy]
   def index
     @users = User.all.page(params[:page]).per(1)
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def new
     @user = User.new
@@ -23,14 +24,25 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    @user = current_user
+    if @user.update_attributes(user_params)
+      flash[:success] = 'ユーザ情報を更新しました'
+      redirect_to @user
+    else
+        flash[:danger] = 'ユーザ情報の更新に失敗しました'
+        render :edit
+    end
   end
 
   def destroy
+    current_user.destroy
+    flash[:success] = '退会しました'
+    redirect_to :root
   end
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
@@ -38,5 +50,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+  #ゲストユーザかどうかの判定
+  def guest_user?
+    if current_user.email = 'guest@user.com' && current_user.password = password = 'guestuser'
+      flash[:warning] = 'ゲストユーザーの情報を変更したり削除することはできません'
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
