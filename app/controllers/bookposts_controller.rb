2238df5a1ad4f01c8f1d3fb7347b7b67fccdd313
rfujisaki <sax.friza2421db@gmail.com>
2020-12-11 21:21:07 +0900

    simple bookpost edit&deestroy

--- app/controllers/bookposts_controller.rb
+++ app/controllers/bookposts_controller.rb
@@ -1,10 +1,11 @@
 class BookpostsController < ApplicationController
+  before_action :set_bookpost, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   def index
     @bookposts = Bookpost.all.page(params[:page]).per(5)
   end
 
   def show
-    @bookpost = Bookpost.find(params[:id])
   end
 
   def new
@@ -26,12 +27,26 @@ class BookpostsController < ApplicationController
   end
 
   def update
+    if @bookpost.update_attributes(bookpost_params)
+      flash[:success] = '投稿内容を更新しました'
+      redirect_to @bookpost
+    else
+      flash[:danger] = '更新に失敗しました'
+      render :edit
+    end
   end
 
   def destroy
+    @bookpost.destroy
+    flash[:success] = '投稿を削除しました'
+    redirect_to action: :index
   end
 
   def bookpost_params
     params.require(:bookpost).permit(:title, :description, :category)
+  end
+
+  def set_bookpost
+    @bookpost = current_user.bookposts.find_by(id: params[:id])
   end
 end
