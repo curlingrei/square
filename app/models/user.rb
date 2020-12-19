7f80808874039390e9ac99beedeed90c34c20e79
rfujisaki <sax.friza2421db@gmail.com>
2020-12-19 19:07:04 +0900

    add booklike method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   has_many :booklikes
 
+  def booklike(bookpost)
+      self.booklikes.find_or_create_by(bookpost_id: bookpost.id) unless self.bookposts.include?(bookpost)
+  end
+
+  def bookdislike(bookpost)
+    target_booklike = self.booklikes.find_by(bookpost_id: bookpost.id)
+    target_booklike.destroy if target_booklike
+  end
+
+  def booklike?(bookpost)
+    self.booklikes.exists?(bookpost_id: bookpost.id)
+  end
 end
