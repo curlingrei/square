9e9233b2504d3693a2a62c23fada62d6a5be2c5e
rfujisaki <sax.friza2421db@gmail.com>
2020-12-28 12:03:35 +0900

    finish edit update destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,15 +27,25 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update_attributes(groups_params)
+      flash[:success] = 'グループの情報を更新しました'
+      redirect_to @group
+    else
+       flash[:danger] = 'グループ情報の更新に失敗しました'
+       render :edit
+    end
   end
 
   def destroy
+    @group.destroy
+    flash[:success] = 'グループを削除しました'
+    redirect_to groups_url
   end
 
   private
 
   def groups_params
-    params.require(:group).permit(:group_name, :first_category, :second_category, :group_description, :default_img_ptn, :image, :remove_image, :user_id)
+    params.require(:group).permit(:group_name, :first_category, :group_description, :default_img_ptn, :image, :remove_image, :user_id)
   end
 
   def set_group
