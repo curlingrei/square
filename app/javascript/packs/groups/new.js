a59ac65a752f21e11d54ca5cf6cbb67d5bf4dddb
rfujisaki <sax.friza2421db@gmail.com>
2020-12-31 16:10:16 +0900

    modify form_design

--- app/javascript/packs/groups/new.js
+++ app/javascript/packs/groups/new.js
@@ -1,14 +1,5 @@
 import "stylesheets/groups/new.scss";
-
-// value属性の値を取得
-// jsの方でvalueが読書だったら、~~を表示させると言うコードをかく
-// 中カテゴリは元々非表示にする
-// アクティブクラスをaddしてactiveの物だけ表示させる？
-// アクティブクラスを付与する対象をif value == なんちゃらってできればいいのか
-
-//ラジオボタンっぽいのを作って、選択した時に画像画像選択フィールドのvalueに値をセットする形にしてみよう
-
-
+require("@rails/ujs").start()
 $(function(){
     //画像選択
     $('.c-img').click(function(){
